2f23b2d70b1b059fd012d3afa03e5a57719be4a5
Nathan Hammond <nathan@nathanhammond.com>
2014-04-09 22:41:43 -0400

    Do what @joliss says.

--- Brocfile.js
+++ Brocfile.js
@@ -89,7 +89,7 @@ module.exports = function (broccoli) {
 
   // Styles
 
-  var styles = preprocessCss(appAndDependencies.inputTrees, prefix + '/styles', '/assets');
+  var styles = preprocessCss(sourceTrees, prefix + '/styles', '/assets');
 
   // Ouput
 
