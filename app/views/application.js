9d8812d104302c04df471e1032c30ba153da7e05
Nathan Hammond <nathan@nathanhammond.com>
2014-04-09 21:16:20 -0400

    First pass at allowing pane resizing. There might be a more Ember-like method of accomplishing this, but I don't know it. Issues with flexbox "over-sizing" still need to be addressed.

--- app/views/application.js
+++ app/views/application.js
@@ -1,3 +1,48 @@
 export default Ember.View.extend({
-  classNames: ['application']
+  classNames: ['application'],
+
+  resizing: false,
+  resizingDirection: undefined,
+  resizingElem: undefined,
+
+  mouseDown: function(e) {
+    if (e.target.classList.contains('resize')) {
+      e.preventDefault();
+      this.set('resizing', true);
+      this.set('resizingDirection', e.target.parentNode.classList.contains('workspace') ? 'row' : 'column');
+      this.set('resizingElem', e.target);
+    }
+  },
+  mouseMove: function(e) {
+    if (this.get('resizing')) {
+      e.preventDefault();
+
+      var dimension, measure, cursor;
+      if (this.get('resizingDirection') == 'column') {
+        dimension = "width";
+        measure = "right";
+        cursor = e.pageX;
+      } else {
+        dimension = "height";
+        measure = "bottom";
+        cursor = e.pageY;
+      }
+
+      var elem = this.get('resizingElem');
+      var previous = elem.previousElementSibling;
+      var next = elem.nextElementSibling;
+
+      var previousrect = previous.getBoundingClientRect();
+      var nextrect = next.getBoundingClientRect();
+
+      var newpreviousdimension = previousrect[dimension] - (previousrect[measure] - cursor);
+      var newnextdimension = nextrect[dimension] + (previousrect[measure] - cursor);
+
+      previous.style[dimension] = newpreviousdimension + 'px';
+      next.style[dimension] = newnextdimension + 'px';
+    }
+  },
+  mouseUp: function(e) {
+    this.set('resizing', false);
+  }
 });
