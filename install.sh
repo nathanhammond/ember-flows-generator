d31d1f54857ab2b631621b378a5d47cb1a272684
Nathan Hammond <nathan@nathanhammond.com>
2014-04-16 13:11:28 -0400

    Move the the loop-protect library.

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 rm -rf vendor node_modules tmp
 bower install
 npm install
-cp node_modules/jsbin/public/js/runner/loop-protect.js vendor/loop-protect.js
 cp node_modules/es6-module-transpiler/dist/es6-module-transpiler.js vendor/es6-module-transpiler.js
 cp vendor/normalize-css/normalize.css app/styles/normalize.scss
 cp vendor/codemirror/lib/codemirror.css app/styles/components/code-editor/codemirror.scss
