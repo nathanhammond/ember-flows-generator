e52988d4a8767ddc702c46b2244c573a11b8104f
Nathan Hammond <nathan@nathanhammond.com>
2014-04-17 15:35:16 -0400

    Add automated deploy script.

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Make sure we're in a good state.
+git checkout master
+rm -rf dist
+~/repos/ember-cli/bin/ember build production
+
+# Move to the "release" branch and make everything work.
+git checkout gh-pages
+ls -1 | grep -v -E '^dist$' | xargs rm -rf
+mv dist/* ./
+mv dist/.* ./
+rmdir dist
+
+# Add everything and push it.
+git commit -am "Update."
+git push
+git checkout master
+./install.sh
