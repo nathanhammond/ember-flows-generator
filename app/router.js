87400b524b2b0fe89d4089da141186d69f7b5637
Nathan Hammond <nathan@nathanhammond.com>
2014-04-21 18:02:21 -0400

    Individual sections can be displayed as a whole or a route.

--- app/router.js
+++ app/router.js
@@ -1,9 +1,14 @@
 var Router = Ember.Router.extend({
   rootURL: ENV.rootURL,
-  location: 'none'
+  location: 'hash'
 });
 
 Router.map(function() {
+  this.route('state');
+  this.route('stepper');
+  this.route('router');
+  this.route('canvas');
+  this.route('details');
 });
 
 export default Router;
