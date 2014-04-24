e8136502e1fee93387856bce31ddb1d6a31f38a3
Nathan Hammond <nathan@nathanhammond.com>
2014-04-24 14:12:04 -0400

    ember init https://github.com/stefanpenner/ember-cli/commit/a48764b352f9de17fb1de061ed11a8c6f107afe9

--- Brocfile.js
+++ Brocfile.js
@@ -1,190 +1,67 @@
 /* global require, module */
 
-var uglifyJavaScript = require('broccoli-uglify-js');
-var replace = require('broccoli-replace');
-var compileES6 = require('broccoli-es6-concatenator');
-var validateES6 = require('broccoli-es6-import-validate');
-var pickFiles = require('broccoli-static-compiler');
-var mergeTrees = require('broccoli-merge-trees');
-var findBowerTrees = require('broccoli-bower');
-
 var env = require('broccoli-env').getEnv();
-var getEnvJSON = require('./config/environment');
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var push = Array.prototype.push;
 
-var p = require('ember-cli/lib/preprocessors');
-var preprocessCss = p.preprocessCss;
-var preprocessTemplates = p.preprocessTemplates;
-var preprocessJs = p.preprocessJs;
+var app = new EmberApp(env, {
+  name: require('./package.json').name,
 
-module.exports = function (broccoli) {
-
-  var prefix = 'ember-flows-generator';
-  var rootURL = '/';
-
-  // index.html
-
-  var indexHTML = pickFiles('app', {
-    srcDir: '/',
-    files: ['index.html'],
-    destDir: '/'
-  });
-
-  indexHTML = replace(indexHTML, {
-    files: ['index.html'],
-    patterns: [{ match: /\{\{ENV\}\}/g, replacement: getEnvJSON.bind(null, env)}]
-  });
-
-  // sourceTrees, appAndDependencies for CSS and JavaScript
-
-  var app = pickFiles('app', {
-    srcDir: '/',
-    destDir: prefix
-  });
-
-  app = preprocessTemplates(app);
-
-  var config = pickFiles('config', { // Don't pick anything, just watch config folder
-    srcDir: '/',
-    files: [],
-    destDir: '/'
-  });
-
-  var sourceTrees = [app, config, 'vendor'].concat(findBowerTrees());
-  var appAndDependencies = mergeTrees(sourceTrees, { overwrite: true });
-
-  // JavaScript
-
-  var legacyFilesToAppend = [
+  legacyFilesToAppend: [
     'jquery.js',
     'handlebars.js',
     'ember.js',
     'ic-ajax/dist/named-amd/main.js',
     'ember-data.js',
+    'app-shims.js',
     'ember-resolver.js',
     'ember-load-initializers.js',
-    'ember-shim.js',
     'codemirror/lib/codemirror.js',
     'codemirror/mode/javascript/javascript.js',
     'es6-module-transpiler.js',
     'esprima.js',
     'escodegen.browser.js',
     'loop-protect.js'
-  ];
+  ],
 
-  var applicationJs = preprocessJs(appAndDependencies, '/', prefix);
+  // AKA whitelisted modules
+  ignoredModules: [
+    'ember',
+    'ember/resolver',
+    'ember/load-initializers',
+    'ic-ajax'
+  ],
 
-  applicationJs = compileES6(applicationJs, {
-    loaderFile: 'loader/loader.js',
-    ignoredModules: [
-      'ember/resolver',
-      'ember/load-initializers',
-      'ic-ajax'
+  // hack we can hopefully remove as the addon system improves
+  importWhitelist: {
+    'ember': ['default'],
+    'ember/resolver': ['default'],
+    'ember/load-initializers': ['default'],
+    'ember-qunit': [
+      'globalize',
+      'moduleFor',
+      'moduleForComponent',
+      'moduleForModel',
+      'test',
+      'setResolver'
     ],
-    inputFiles: [
-      prefix + '/**/*.js'
-    ],
-    legacyFilesToAppend: legacyFilesToAppend,
-    wrapInEval: env !== 'production',
-    outputFile: '/assets/app.js'
-  });
+    'qunit': ['default']
+  },
 
-  if (env === 'production') {
-    applicationJs = uglifyJavaScript(applicationJs, {
-      mangle: false,
-      compress: false
-    });
-  }
+  // hack
+  getEnvJSON: require('./config/environment')
+});
 
-  // Styles
+if (env !== 'production') {
+  push.apply(app.ignoredModules, [
+    'qunit',
+    'ember-qunit'
+  ]);
 
-  var styles = preprocessCss(appAndDependencies, prefix + '/styles', '/assets');
+  push.apply(app.legacyFilesToAppend, [
+    'test-shims.js',
+    'ember-qunit/dist/named-amd/main.js'
+  ]);
+}
 
-  // Ouput
-
-  var outputTrees = [
-    indexHTML,
-    applicationJs,
-    'public',
-    styles
-  ];
-
-  // Testing
-
-  if (env !== 'production') {
-
-    var tests = pickFiles('tests', {
-      srcDir: '/',
-      destDir: prefix + '/tests'
-    });
-
-    var testsIndexHTML = pickFiles('tests', {
-      srcDir: '/',
-      files: ['index.html'],
-      destDir: '/tests'
-    });
-
-    var qunitStyles = pickFiles('vendor', {
-      srcDir: '/qunit/qunit',
-      files: ['qunit.css'],
-      destDir: '/assets/'
-    });
-
-    testsIndexHTML = replace(testsIndexHTML, {
-      files: ['tests/index.html'],
-      patterns: [{ match: /\{\{ENV\}\}/g, replacement: getEnvJSON.bind(null, env)}]
-    });
-
-    tests = preprocessTemplates(tests);
-
-    sourceTrees = [tests, 'vendor'].concat(findBowerTrees());
-    appAndDependencies = mergeTrees(sourceTrees, { overwrite: true });
-
-    var testsJs = preprocessJs(appAndDependencies, '/', prefix);
-
-    var validatedJs = validateES6(mergeTrees([app, tests]), {
-      whitelist: {
-        'ember/resolver': ['default'],
-        'ember/load-initializers': ['default'],
-        'ember-qunit': [
-          'globalize',
-          'moduleFor',
-          'moduleForComponent',
-          'moduleForModel',
-          'test',
-          'setResolver'
-        ]
-      }
-    });
-
-    var legacyTestFiles = [
-      'qunit/qunit/qunit.js',
-      'qunit-shim.js',
-      'ember-qunit/dist/named-amd/main.js'
-    ];
-
-    legacyFilesToAppend = legacyFilesToAppend.concat(legacyTestFiles);
-
-    testsJs = compileES6(testsJs, {
-      // Temporary workaround for
-      // https://github.com/joliss/broccoli-es6-concatenator/issues/9
-      loaderFile: '_loader.js',
-      ignoredModules: [
-        'ember/resolver',
-        'ember/load-initializers',
-        'ember-qunit'
-      ],
-      inputFiles: [
-        prefix + '/**/*.js'
-      ],
-      legacyFilesToAppend: legacyFilesToAppend,
-
-      wrapInEval: true,
-      outputFile: '/assets/tests.js'
-    });
-
-    var testsTrees = [qunitStyles, testsIndexHTML, validatedJs, testsJs];
-    outputTrees = outputTrees.concat(testsTrees);
-  }
-
-  return mergeTrees(outputTrees, { overwrite: true });
-};
+module.exports = app.toTree();
