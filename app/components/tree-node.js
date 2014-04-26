323a2f7bea18551a12d6be0ee7d932c51015c578
Nathan Hammond <nathan@nathanhammond.com>
2014-04-25 20:30:01 -0400

    Prep for SVG drag/drop.

--- app/components/tree-node.js
+++ app/components/tree-node.js
@@ -6,9 +6,17 @@ export default Ember.Component.extend({
   dragging: false,
 
   dragproxy: function() {
-    var dragproxy = document.createElement('div');
-    dragproxy.className = 'dragproxy'
-    dragproxy.innerHTML = this.get('node.value.name');
+    var name = this.get('node.value.name');
+    var splits = name.split(/([\.\_\-]{1})/);
+    splits = splits.map(function(item) { return "<tspan>" + item + "</tspan>"; });
+
+    var dragproxy = document.createElement('svg');
+    dragproxy.setAttribute("version", "1.1");
+    dragproxy.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    dragproxy.className = 'dragproxy';
+
+    dragproxy.innerHTML = "<text>"+ splits.join('') +"</text>"
+
     return dragproxy;
   }.property(),
 
@@ -41,8 +49,11 @@ export default Ember.Component.extend({
       this.set('dragging', true);
       document.body.classList.add('dragging');
 
-      dragproxy.style.top = e.pageY - (dims.height/2) + 'px';
-      dragproxy.style.left = e.pageX - (dims.width/2) + 'px';
+      var x = e.pageX - (dims.width/2) + 'px';
+      var y = e.pageY - (dims.height/2) + 'px'
+
+      dragproxy.style.webkitTransform = "translate("+x+", "+y+")";
+      dragproxy.style.transform = "translate("+x+", "+y+")";
     }.bind(this);
 
     var mouseUpHandler = function(e) {
@@ -54,8 +65,8 @@ export default Ember.Component.extend({
 
       // Clean up
       document.body.removeChild(dragproxy);
-      dragproxy.style.top = '';
-      dragproxy.style.left = '';
+      dragproxy.style.webkitTransform = '';
+      dragproxy.style.transform = '';
 
       this.set('dragging', false);
     }.bind(this);
