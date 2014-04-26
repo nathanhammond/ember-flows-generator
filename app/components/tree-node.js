8ec52e71dfccaf07ee3c1ab02d4bab814ebb537e
Nathan Hammond <nathan@nathanhammond.com>
2014-04-26 02:57:00 -0400

    Make dragproxy real. Still need to implement the rest of my text layout algorithm for multiple-line nodes. Still need to implement the "minimum" and "maximum" size node handling with viewbox.

--- app/components/tree-node.js
+++ app/components/tree-node.js
@@ -5,18 +5,107 @@ export default Ember.Component.extend({
 
   dragging: false,
 
+  getBaselineRatio: function() {
+    var container = document.createElement('div');
+    var small = document.createElement('span');
+    var large = document.createElement('span');
+
+    container.style.visibility = "hidden";
+    small.style.fontSize = "0px";
+    large.style.fontSize = "1800px";
+
+    small.innerHTML = "X";
+    large.innerHTML = "X";
+
+    container.appendChild(small);
+    container.appendChild(large);
+
+    document.body.appendChild(container);
+    var smalldims = small.getBoundingClientRect();
+    var largedims = large.getBoundingClientRect();
+    var baselineposition = smalldims.top - largedims.top;
+    var height = largedims.height;
+    document.body.removeChild(container);
+
+    return 1 - (baselineposition / height);
+  },
+
   dragproxy: function() {
+    var svgnamespace = 'http://www.w3.org/2000/svg';
     var name = this.get('node.value.name');
     var splits = name.split(/([\.\_\-]{1})/);
-    splits = splits.map(function(item) { return "<tspan>" + item + "</tspan>"; });
+    splits = splits.map(function(item) { return '<tspan style="text-anchor: middle;">' + item + "</tspan>"; });
 
-    var dragproxy = document.createElement('svg');
+    var dragproxy = document.createElementNS(svgnamespace, "svg");
     dragproxy.setAttribute("version", "1.1");
-    dragproxy.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    dragproxy.className = 'dragproxy';
+    dragproxy.setAttribute('class', 'dragproxy');
 
-    dragproxy.innerHTML = "<text>"+ splits.join('') +"</text>"
+    var circle = document.createElementNS(svgnamespace, 'circle');
+    circle.setAttribute('fill', 'green');
 
+    var text = document.createElementNS(svgnamespace, 'text');
+    text.innerHTML = splits.join('');
+
+    // Much easier than instantiating them individually. :)
+    var tspans = [].slice.call(text.getElementsByTagName('tspan'));
+
+    dragproxy.appendChild(circle);
+    dragproxy.appendChild(text);
+
+    document.body.appendChild(dragproxy);
+
+    /* Now that it's real, let's do some calculations on it to lay it out correctly. */
+    var absoluteminheight = tspans[0].getBoundingClientRect().height;
+    var absolutemaxheight = absoluteminheight * tspans.length;
+
+    var absolutemaxwidth = text.getBoundingClientRect().width;
+    var absoluteminwidth = tspans.reduce(function(previousmin, elem) {
+      return Math.min(previousmin, elem.getBoundingClientRect().width);
+    }, absolutemaxwidth);
+
+    var baselineshift = this.getBaselineRatio() * absoluteminheight;
+
+    var radius, numrows;
+    if (tspans.length === 1) {
+      // Account for the fact that the tspan is rectangular, not linear.
+      var a = absolutemaxwidth / 2;
+      var b = absolutemaxheight / 2;
+      radius = Math.sqrt(a*a+b*b);
+      numrows = 1;
+    } else {
+      // FIXME: Make this actually search through the space.
+      // FIXME: Account for the fact that the tspan is rectangular, not linear.
+      var searchrange = {
+        min: Math.min(absoluteminwidth, absoluteminheight),
+        max: Math.max(absolutemaxwidth, absolutemaxheight)
+      }
+
+      var a = absolutemaxwidth / 2;
+      var b = absolutemaxheight / 2;
+      radius = Math.sqrt(a*a+b*b);
+      numrows = 1;
+    }
+
+    // Vertically centering the tspans in the circle means knowing how tall they are.
+    var topspace = ((radius * 2) - (numrows * absoluteminheight))/2;
+
+    circle.setAttribute('cx', radius);
+    circle.setAttribute('cy', radius);
+    circle.setAttribute('r', radius);
+    text.setAttribute('x', 0);
+    text.setAttribute('y', topspace + absoluteminheight - baselineshift);
+
+    tspans.forEach(function(tspan, index) {
+      tspan.setAttribute('x', radius);
+      if (index) {
+        tspan.setAttribute('dy', absoluteminheight);
+      }
+    });
+
+    document.body.removeChild(dragproxy);
+
+    dragproxy.setAttribute('width', (radius*2)+'px');
+    dragproxy.setAttribute('height', (radius*2)+'px')
     return dragproxy;
   }.property(),
 
