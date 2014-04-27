b92fd48c0f687ee5aeaff7fa45c2a379730524c1
Nathan Hammond <nathan@nathanhammond.com>
2014-04-27 07:18:53 -0400

    Permute returns an array of possibilities.

--- app/permute.js
+++ app/permute.js
@@ -2,13 +2,20 @@ function permute(splits, depth, windowsize) {
   windowsize = windowsize || splits.length
   depth = depth || 0;
 
-  var results = {};
+  // Handle all but the "1" case.
+  var results = [];
   for (var i = windowsize; i > 1; i--) {
-    results[i] = process(splits, depth, i);
+    var result = process(splits, depth, i);
+    results = [].concat(results, result);
   }
 
-  // Handle the easy case.
-  results["1"] = [ { current: splits } ];
+  // Handle the "1" case.
+  results.push(splits);
+
+  // Flatten things unless it's the first.
+  if (results.length === 1 && depth !== 0) {
+    results = results[0];
+  }
   return results;
 }
 
@@ -19,7 +26,11 @@ function process(splits, depth, windowsize) {
 
   // We're going to try this once, no matter what.
   for (var offset = 0; offset <= shifts; offset++) {
-    var result = {};
+    var result = {
+      before: [],
+      current: [],
+      after: []
+    };
 
     // If there is space before the sliding window, permute it.
     if (offset !== 0) { result.before = permute(splits.slice(0, offset), depth + 1); }
@@ -30,7 +41,25 @@ function process(splits, depth, windowsize) {
     // If there is space after the sliding window, permute it.
     if (offset + windowsize < splits.length) { result.after = permute(splits.slice(offset + windowsize), depth + 1); }
 
-    results.push(result);
+    // Flatten the results.
+    if (result.before.length && result.after.length) {
+      for (var beforeoption in result.before) {
+        for (var afteroption in result.after) {
+          results.push([].concat(result.before[beforeoption], result.current, result.after[afteroption]));
+        }
+      }
+    } else if (result.before.length) {
+      for (var beforeoption in result.before) {
+        results.push([].concat(result.before[beforeoption], result.current));
+      }
+    } else if (result.after.length) {
+      for (var afteroption in result.after) {
+        results.push([].concat(result.current, result.after[afteroption]));
+      }
+    } else {
+      results.push(result.current);
+    }
+
   }
 
   return results;
