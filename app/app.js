b10165e0e7c343cc31c750818b3c53b11f89e8ed
Nathan Hammond <nathan@nathanhammond.com>
2014-04-27 12:12:38 -0400

    Remove ember-cli TODOs from the repo.

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ import loadInitializers from 'ember/load-initializers';
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
-  modulePrefix: 'ember-flows-generator', // TODO: loaded via config
+  modulePrefix: 'ember-flows-generator',
   Resolver: Resolver
 });
 
--- tests/test-loader.js
+++ tests/test-loader.js
@@ -1,4 +1,3 @@
-// TODO: load based on params
 Ember.keys(requirejs._eak_seen).filter(function(key) {
   return (/\-test/).test(key);
 }).forEach(function(moduleName) {
