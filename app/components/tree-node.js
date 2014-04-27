bbd888c7a77d434daff75860d5ef13b3240317bc
Nathan Hammond <nathan@nathanhammond.com>
2014-04-27 12:16:08 -0400

    Save a mostly-good version of circular text-fitting.

--- app/components/tree-node.js
+++ app/components/tree-node.js
@@ -1,3 +1,76 @@
+var permute = (function() {
+  function permute(splits, depth, windowsize) {
+    windowsize = windowsize || splits.length
+    depth = depth || 0;
+
+    // Handle all but the "1" case.
+    var results = [];
+    for (var i = windowsize; i > 1; i--) {
+      var result = process(splits, depth, i);
+      results = [].concat(results, result);
+    }
+
+    // Handle the "1" case.
+    results.push(splits);
+
+    // Flatten things unless it's the first.
+    if (results.length === 1 && depth !== 0) {
+      results = results[0];
+    }
+
+    // FIXME: Guarantee unique values.
+    return results;
+  }
+
+  function process(splits, depth, windowsize) {
+    var shifts = splits.length - windowsize;
+
+    var results = [];
+
+    // We're going to try this once, no matter what.
+    for (var offset = 0; offset <= shifts; offset++) {
+      var result = {
+        before: [],
+        current: [],
+        after: []
+      };
+
+      // If there is space before the sliding window, permute it.
+      if (offset !== 0) { result.before = permute(splits.slice(0, offset), depth + 1); }
+
+      // Save the current sliding window every time.
+      result.current = [ splits.slice(offset, offset + windowsize) ];
+
+      // If there is space after the sliding window, permute it.
+      if (offset + windowsize < splits.length) { result.after = permute(splits.slice(offset + windowsize), depth + 1); }
+
+      // Flatten the results.
+      if (result.before.length && result.after.length) {
+        for (var beforeoption in result.before) {
+          for (var afteroption in result.after) {
+            results.push([].concat(result.before[beforeoption], result.current, result.after[afteroption]));
+          }
+        }
+      } else if (result.before.length) {
+        for (var beforeoption in result.before) {
+          results.push([].concat(result.before[beforeoption], result.current));
+        }
+      } else if (result.after.length) {
+        for (var afteroption in result.after) {
+          results.push([].concat(result.current, result.after[afteroption]));
+        }
+      } else {
+        results.push(result.current);
+      }
+
+    }
+
+    return results;
+  }
+
+  return permute;
+})();
+
 export default Ember.Component.extend({
   tagName: 'li',
   classNames: ['tree-node'],
@@ -36,13 +109,14 @@ export default Ember.Component.extend({
 
     var svgnamespace = 'http://www.w3.org/2000/svg';
     var name = this.get('node.value.name');
-    var splits = name.split(/([\.\_\-]{1})/);
-    var tspans = splits.map(function(split) {
-      var tspan = document.createElementNS(svgnamespace, 'tspan');
-      tspan.style.textAnchor = "middle";
-      tspan.innerHTML = split;
-      return tspan;
-    });
+
+    var splits = name.split(/([\.\_\-]+)/);
+
+    // Rejoin the leading item with the split character(s).
+    for (var i = 1; i < splits.length; i++) {
+      splits[i-1] = splits[i-1] + splits[i];
+      splits.splice(i, 1);
+    }
 
     var dragproxy = document.createElementNS(svgnamespace, "svg");
     dragproxy.setAttribute("version", "1.1");
@@ -52,6 +126,13 @@ export default Ember.Component.extend({
     circle.setAttribute('fill', 'green');
 
     var text = document.createElementNS(svgnamespace, 'text');
+
+    var tspans = splits.map(function(split) {
+      var tspan = document.createElementNS(svgnamespace, 'tspan');
+      tspan.style.textAnchor = "middle";
+      tspan.innerHTML = split;
+      return tspan;
+    });
     tspans.forEach(function(tspan) {
       text.appendChild(tspan);
     });
@@ -62,39 +143,52 @@ export default Ember.Component.extend({
     document.body.appendChild(dragproxy);
 
     /* Now that it's real, let's do some calculations on it to lay it out correctly. */
-    var absoluteminheight = tspans[0].getBoundingClientRect().height;
-    var absolutemaxheight = absoluteminheight * tspans.length;
-
-    var absolutemaxwidth = text.getBoundingClientRect().width;
-    var absoluteminwidth = tspans.reduce(function(previousmin, elem) {
-      return Math.min(previousmin, elem.getBoundingClientRect().width);
-    }, absolutemaxwidth);
-
-    var baselineshift = this.getBaselineRatio() * absoluteminheight;
+    var tspanheight = tspans[0].getBoundingClientRect().height;
+    var baselineshift = this.getBaselineRatio() * tspanheight;
 
     // Workaround for Firefox bug https://bugzilla.mozilla.org/show_bug.cgi?id=1001804
     if (/firefox/i.test(navigator.userAgent)) { baselineshift = 0; }
 
-    var radius, numrows;
-    if (tspans.length === 1) {
-      // Account for the fact that the tspan is rectangular, not linear.
-      var a = absolutemaxwidth / 2;
-      var b = absolutemaxheight / 2;
-      radius = Math.sqrt(a*a+b*b);
-      numrows = 1;
-    } else {
-      // FIXME: Make this actually search through the space.
-      // FIXME: Account for the fact that the tspan is rectangular, not linear.
-      var searchrange = {
-        min: Math.min(absoluteminwidth, absoluteminheight),
-        max: Math.max(absolutemaxwidth, absolutemaxheight)
+    var permutations = permute(tspans);
+    var radius, numrows, solution;
+    permutations.forEach(function(permutation) {
+      var radii = [];
+      var rows = permutation.length;
+
+      var a, b, multiplier, tspan;
+      for (var row = 0; row < rows; row++) {
+
+        // A will be the height of the number of rows from center.
+        multiplier = rows/2 - row;
+        multiplier = multiplier > 0 ? multiplier : Math.abs(multiplier-1);
+        a = multiplier * tspanheight;
+
+        // B will be half the horizontal width.
+        if (permutation[row] instanceof Array) {
+          b = permutation[row].reduce(function(previous, tspan) {
+            /* Chrome has a bug where it can't accurately get SVG width using getBoundingClientRect(). */
+            /* Firefox returns undefined for offsetWidth on SVG elements. */
+            /* When these powers combine: */
+            var width = tspan.offsetWidth || tspan.getBoundingClientRect().width;
+            return width + previous;
+          }, 0) / 2;
+        } else {
+          tspan = permutation[row];
+          b = (tspan.offsetWidth || tspan.getBoundingClientRect().width) / 2;
+        }
+
+        radii.push(Math.sqrt(a*a+b*b));
       }
 
-      var a = absolutemaxwidth / 2;
-      var b = absolutemaxheight / 2;
-      radius = Math.sqrt(a*a+b*b);
-      numrows = 1;
-    }
+      var largest = Math.max.apply(Math, radii);
+      if (!radius || radius >= largest) {
+        radius = largest;
+        numrows = permutation.length;
+        solution = permutation;
+      }
+    });
+
+    // TODO: Process the solution and merge the tspans.
 
     if (radius + radiuspadding > minradius) {
       // Add the padding at the right scale.
@@ -105,18 +199,18 @@ export default Ember.Component.extend({
     }
 
     // Vertically centering the tspans in the circle means knowing how tall they are.
-    var topspace = ((radius * 2) - (numrows * absoluteminheight))/2;
+    var topspace = ((radius * 2) - (numrows * tspanheight))/2;
 
     circle.setAttribute('cx', radius);
     circle.setAttribute('cy', radius);
     circle.setAttribute('r', radius);
     text.setAttribute('x', 0);
-    text.setAttribute('y', topspace + absoluteminheight - baselineshift);
+    text.setAttribute('y', topspace + tspanheight - baselineshift);
 
     tspans.forEach(function(tspan, index) {
       tspan.setAttribute('x', radius);
       if (index) {
-        tspan.setAttribute('dy', absoluteminheight);
+        tspan.setAttribute('dy', tspanheight);
       }
     });
 
