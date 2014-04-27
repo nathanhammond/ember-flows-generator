0394970b1a899a34f44d644e1e346cd4501d6f93
Nathan Hammond <nathan@nathanhammond.com>
2014-04-27 06:07:59 -0400

    Add depth tracking.

--- app/permute.js
+++ app/permute.js
@@ -1,9 +1,10 @@
-function permute(splits, windowsize) {
+function permute(splits, depth, windowsize) {
   windowsize = windowsize || splits.length
+  depth = depth || 0;
 
   var results = {};
   for (var i = windowsize; i > 1; i--) {
-    results[i] = process(splits, i);
+    results[i] = process(splits, depth, i);
   }
 
   // Handle the easy case.
@@ -11,7 +12,7 @@ function permute(splits, windowsize) {
   return results;
 }
 
-function process(splits, windowsize) {
+function process(splits, depth, windowsize) {
   var shifts = splits.length - windowsize;
 
   var results = [];
@@ -21,13 +22,13 @@ function process(splits, windowsize) {
     var result = {};
 
     // If there is space before the sliding window, permute it.
-    if (offset !== 0) { result.before = permute(splits.slice(0, offset)); }
+    if (offset !== 0) { result.before = permute(splits.slice(0, offset), depth + 1); }
 
     // Save the current sliding window every time.
     result.current = [ splits.slice(offset, offset + windowsize).join('') ];
 
     // If there is space after the sliding window, permute it.
-    if (offset + windowsize < splits.length) { result.after = permute(splits.slice(offset + windowsize)); }
+    if (offset + windowsize < splits.length) { result.after = permute(splits.slice(offset + windowsize), depth + 1); }
 
     results.push(result);
   }
