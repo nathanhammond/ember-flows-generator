a685a61f11173fa94a3c52d0eeb521d5e11efa39
Nathan Hammond <nathan@nathanhammond.com>
2014-04-27 12:14:17 -0400

    Make multi-level testing easier.

--- app/controllers/router.js
+++ app/controllers/router.js
@@ -55,7 +55,7 @@ function arrayizeChildren(routeTree) {
 
 export default Ember.Controller.extend({
   flipped: false,
-  code: "var Router = Ember.Router.extend({\n  rootURL: ENV.rootURL,\n  location: 'auto'\n});\n\nRouter.map(function() {\n  this.route('asdf');\n});\n\nexport default Router;",
+  code: "var Router = Ember.Router.extend({\n  rootURL: ENV.rootURL,\n  location: 'auto'\n});\n\nRouter.map(function() {\n  this.resource('asdf', function() {});\n});\n\nexport default Router;",
 
   changeCount: 0,
 
